30f6a53e67ae9bc03712c9d61dd78572fdcd76cf
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2025-01-02 23:49:01 -0300

    learning!@

---
+++ combat.py
@@ -0,0 +1,7 @@
+damage_sword = 10
+health_player = 100
+hp_after_attack = health_player - damage_sword
+
+print(f"Slw health is: {health_player}")
+print(f"Slw has been hited by a sword for {damage_sword} damage...")
+print(f"Slw health now is: {hp_after_attack}")
