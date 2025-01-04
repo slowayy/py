32b55deb854f5c95b3c969ce2a67a37fc2294da9
Gabriel Alcântara <gabrielqalcantara@gmail.com>
2025-01-03 23:13:04 -0300

    learning!@

---
+++ failure.py
@@ -0,0 +1,3 @@
+print("Starting up game server...")
+# prnt("local game server is listening on port 6969")
+print("local game server is listening on port 9494")
---
+++ response.py
@@ -0,0 +1,2 @@
+print("Ah! Great choices...")
+print("Is there anything els I can help you with?")
